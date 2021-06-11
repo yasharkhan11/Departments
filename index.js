5b1e2cb1c2bcf661610cf89d2028a401a9d26527
Yashar Haque <79962551+yasharkhan11@users.noreply.github.com>
2021-06-11 10:44:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function Department1(){
+
+    window.location = 'Dept.of_bussiness&Management.html';
+}
+function Department2(){
+
+    window.location = 'home.html';
+}
+function Department3(){
+
+    window.location = 'home.html';
+}
+function Department4(){
+
+    window.location = 'home.html';
+}
+function Department5(){
+
+    window.location = 'home.html';
+}
+function Department6(){
+
+    window.location = 'home.html';
+}
+function Department7(){
+
+    window.location = 'home.html';
+}
+function Department8(){
+
+    window.location = 'home.html';
+}
+function Department9(){
+
+    window.location = 'home.html';
+}
+function Department10(){
+
+    window.location = 'home.html';
+}
+
+
